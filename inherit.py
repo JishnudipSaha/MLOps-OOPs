b1c58a5d43cae41f4c9719678577657463a6a9ec
JishnudipSaha <jishnudipsaha.10a.15.02062004@gmail.com>
2026-01-18 23:38:55 +0530

    Inheritance completed with super keyword

--- inherit.py
+++ inherit.py
@@ -1,17 +1,41 @@
+# class Animal:
+#     def __init__(self, name):
+#         self.name = name
+    
+#     def action(self):
+#         print(f'{self.name} is in action')
+    
+#     def nameOfAnimal(self):
+#         print(f'The name is {self.name}')
+
+# class Dog(Animal): # Animal class is inherited by Dog
+#     def action(self):
+#         print(f'{self.name} is barking')
+
+# dog = Dog('Doggesh')
+# dog.action()  # Doggesh is in action
+# dog.nameOfAnimal() # since Dog inherits Animal, so Dog class now have all the methods of Animal class
+
+
+# Super keyword use-cases
+
+# base class
 class Animal:
     def __init__(self, name):
         self.name = name
+        
+    def speak(self):
+        print(f"{self.name} is speaking.")
     
-    def action(self):
-        print(f'{self.name} is in action')
+# Derived class
+class Dog(Animal):
+    def __init__(self, name, bh):
+        super().__init__(name)
+        self.bh = bh
     
-    def nameOfAnimal(self):
-        print(f'The name is {self.name}')
+    def speak(self):
+        super().speak()
+        print(f"{self.name} is a very {self.bh} dog.")
 
-class Dog(Animal): # Animal class is inherited by Dog
-    def action(self):
-        print(f'{self.name} is barking')
-
-dog = Dog('Doggesh')
-dog.action()  # Doggesh is in action
-dog.nameOfAnimal() # since Dog inherits Animal, so Dog class now have all the methods of Animal class
+dog = Dog("Golden Retriver", "friendly")
+dog.speak()
