09486c2e71f41283550aea74ffea520cec1de707
JishnudipSaha <jishnudipsaha.10a.15.02062004@gmail.com>
2026-01-19 01:24:59 +0530

    Hiarchical pattern inheritance added

--- adv_inheritance.py
+++ adv_inheritance.py
@@ -22,25 +22,57 @@
 
 #---------------------------------------------------------
 
-# Multi-level inheritance
+# # Multi-level inheritance
 
 
-class GrandParent():
+# class GrandParent():
+#     def __init__(self, name):
+#         self.name = name
+#     def tell_story(self):
+#         print(f"{self.name} is telling a story.")
+
+# class Parent(GrandParent):
+#     def work(self):
+#         print(f"{self.name} is working.")
+
+# class Child(Parent):
+#     def play(self):
+#         print(f"{self.name} is playing.")
+
+# # creting instance of child
+# child = Child("Jisnudip")
+# child.tell_story() # output: Jisnu is telling a story.
+# child.work() # output: Jishnu is working.
+# child.play() # output: Jisnu is playing.
+
+#---------------------------------------------------------
+
+# Hiarchical inheritance
+
+# base class 
+class Parent():
     def __init__(self, name):
         self.name = name
-    def tell_story(self):
-        print(f"{self.name} is telling a story.")
+    def greet(self):
+        print(f"{self.name} is greeting.")
 
-class Parent(GrandParent):
-    def work(self):
-        print(f"{self.name} is working.")
-
-class Child(Parent):
+# derived class 1
+class ChildOne(Parent):
     def play(self):
         print(f"{self.name} is playing.")
+        
+# derived class 2
+class ChildTwo(Parent):
+    def study(self):
+        print(f"{self.name} is studying.")
 
-# creting instance of child
-child = Child("Jisnudip")
-child.tell_story() # output: Jisnu is telling a story.
-child.work() # output: Jishnu is working.
-child.play() # output: Jisnu is playing.
+# creating objects of clid classes
+
+child1 = ChildOne("Jisnudip")
+child2 = ChildTwo("Aritra")
+
+child1.greet() # output: Jishnudip is greeting.
+child1.play() # output: Jishnudip is playing.
+
+child2.greet() # output: Aritra is greeting.
+child2.study() # output: Aritra is studying.
