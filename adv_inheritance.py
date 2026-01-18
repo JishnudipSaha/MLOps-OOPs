3c5da78c95731b6fce4f05e322a499f33cf4947a
JishnudipSaha <jishnudipsaha.10a.15.02062004@gmail.com>
2026-01-19 01:16:05 +0530

    Multi-level ineritance added

--- adv_inheritance.py
+++ adv_inheritance.py
@@ -1,21 +1,46 @@
-# inheritance in python: Single inheritance, multiple inheritance, hybrid inheritance
+# inheritance in python: Single inheritance, multiple inheritance, hiarchical ineritance, hybrid inheritance
 
-# single or basic inheritance
+# # single or basic inheritance
 
-# base class 
-class Parent():
+# # base class 
+# class Parent():
+#     def __init__(self, name):
+#         self.name = name
+    
+#     def greet(self):
+#         print(f"Hello, my name is {self.name}.")
+    
+# # derived class
+# class Child(Parent):
+#     def play(self):
+#         print(f"{self.name} is playing.")
+
+# # Creating instance of a child class
+# child = Child("Jishnu")
+# child.greet() # output: Hello, my name is Baby.
+# child.play() # output: Baby is playing.
+
+#---------------------------------------------------------
+
+# Multi-level inheritance
+
+
+class GrandParent():
     def __init__(self, name):
         self.name = name
-    
-    def greet(self):
-        print(f"Hello, my name is {self.name}.")
-    
-# derived class
+    def tell_story(self):
+        print(f"{self.name} is telling a story.")
+
+class Parent(GrandParent):
+    def work(self):
+        print(f"{self.name} is working.")
+
 class Child(Parent):
     def play(self):
         print(f"{self.name} is playing.")
 
-# Creating instance of a child class
-child = Child("Jishnu")
-child.greet() # output: Hello, my name is Baby.
-child.play() # output: Baby is playing.
+# creting instance of child
+child = Child("Jisnudip")
+child.tell_story() # output: Jisnu is telling a story.
+child.work() # output: Jishnu is working.
+child.play() # output: Jisnu is playing.
